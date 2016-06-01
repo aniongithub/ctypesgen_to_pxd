d27fc43beebd59c7ca9f2e4867b2a34b2f68c315
René Kijewski <rene.kijewski@fu-berlin.de>
2016-06-01 02:06:40 +0200

    pep8 & LICENSE

--- ctypesgen_to_pxd.py
+++ ctypesgen_to_pxd.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
+# encoding: UTF-8
+
+# Copyright 2016 Freie Universität Berlin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 from json import load
 from logging import getLogger, basicConfig
 from re import match
 from sys import stdin, stdout, argv
 
+
+__author__ = 'René Kijewski  <rene.SURNAME@fu-berlin.de>'
+__copyright__ = 'Copyright 2016 Freie Universität Berlin'
+__credits__ = ['René Kijewski']
+__license__ = 'Apache License, Version 2.0'
+__version__ = '0.0.1'
+__maintainer__ = 'René Kijewski'
+__email__ = 'rene.SURNAME@fu-berlin.de'
+__status__ = 'Production'
+
+
+__all__ = ('convert',)
 
 _logger = getLogger('ctypes_to_pxd')
 
@@ -24,7 +52,7 @@ def _format_CtypesSimple(ctype):
         signed = ctype.get('signed')
         longs = ctype.get('longs') or 0
         return (
-            'unsigned ' if not signed else ('signed ' if name == 'int' else ''),
+            'unsigned ' if not signed else 'signed ' if name == 'int' else '',
             'long ' * longs,
             name
         )
@@ -58,9 +86,9 @@ def _format_rhs(definition):
         left = definition.get('left')
         right = definition.get('right')
 
-        if not all( isinstance(i, dict) for i in (left, right) ):
-            _logger.warn('Unknown BinaryExpressionNode type(left)=%r type(right)=%r',
-                         type(left), type(right))
+        if not all(isinstance(i, dict) for i in (left, right)):
+            _logger.warn('Unknown BinaryExpressionNode type(left)=%r '
+                         'type(right)=%r', type(left), type(right))
             return False
 
         name = definition.get('name')
@@ -91,7 +119,8 @@ def _convert_enum(definition, f_out, indent_level):
     fields = definition.get('fields')
 
     if not name or not isinstance(fields, (list, NoneType)):
-        _logger.warn('Unknown enum name=%r type(fields)=%r', name, type(fields))
+        _logger.warn('Unknown enum name=%r type(fields)=%r',
+                     name, type(fields))
         return False
 
     printed = None
@@ -171,7 +200,8 @@ def _convert_function(definition, f_out, indent_level):
 
     returns_args, args_args = args
 
-    _put(f_out, indent_level, 'cdef ', *returns_args, ' ', name, '(', *args_args, ')')
+    _put(f_out, indent_level,
+         'cdef ', *returns_args, ' ', name, '(', *args_args, ')')
     return True
 
 
@@ -204,7 +234,8 @@ def _convert_macro(definition, f_out, indent_level):
             prefix = m.group(2) or m.group(5)
             digits = m.group(3) or m.group(6)
 
-            value = int(digits, 10 if not prefix else 16 if prefix == '0x' else 0)
+            value = int(digits, (10 if not prefix else
+                                 16 if prefix == '0x' else 0))
             if minus:
                 value = -1
 
@@ -220,7 +251,8 @@ def _convert_struct(definition, f_out, indent_level, struct='struct'):
     name = definition.get('name')
     fields = definition.get('fields')
     if not name or not isinstance(fields, (list, NoneType)):
-        _logger.warn('Unknown %s data name=%r type(ctype)=%r', struct, name, type(fields))
+        _logger.warn('Unknown %s data name=%r type(ctype)=%r',
+                     struct, name, type(fields))
         return False
 
     name_args = _anon_struct_name(struct, name)
@@ -229,13 +261,12 @@ def _convert_struct(definition, f_out, indent_level, struct='struct'):
 
     if fields is None:
         _put(f_out, indent_level,
-            'cdef ', struct, ' ', *name_args,
-            '  # forward declaration'
-        )
+             'cdef ', struct, ' ', *name_args, '  # forward declaration')
     else:
         _put(f_out, indent_level, 'cdef ', struct, ' ', *name_args, ':')
         for field in fields:
-            _convert_typedef(field, f_out, indent_level + 1, include_cdef=False)
+            _convert_typedef(field, f_out, indent_level + 1,
+                             include_cdef=False)
 
     return True
 
@@ -285,7 +316,8 @@ def _convert_base_Klass(base):
         tag = base.get('tag')
         variety = base.get('variety')
         if not tag or not variety:
-            _logger.warn('Unsupported base Klass=%r tag=%r variety=%r', klass, tag, variety)
+            _logger.warn('Unsupported base Klass=%r tag=%r variety=%r',
+                         klass, tag, variety)
             return False
 
         return _anon_struct_name(variety, tag)
@@ -293,7 +325,8 @@ def _convert_base_Klass(base):
     elif klass == 'CtypesPointer':
         destination = base.get('destination')
         if not isinstance(destination, dict):
-            _logger.warn('Unsupported base Klass type(destination)=%r', destination)
+            _logger.warn('Unsupported base Klass type(destination)=%r',
+                         destination)
             return False
 
         args = _convert_base_Klass(destination)
@@ -327,8 +360,9 @@ def _convert_base_Klass(base):
 def _convert_typedef_CtypesArray(name, ctype, include_cdef):
     base = ctype.get('base')
     count = ctype.get('count')
-    if not all( isinstance(i, dict) for i in (base, count) ):
-        _logger.warn('CtypesArray type(base)=%r type(count)=%r', type(base), type(count))
+    if not all(isinstance(i, dict) for i in (base, count)):
+        _logger.warn('CtypesArray type(base)=%r type(count)=%r',
+                     type(base), type(count))
         return False
 
     count_args = _format_rhs(count)
@@ -348,7 +382,8 @@ def _convert_typedef_CtypesArray(name, ctype, include_cdef):
 def _convert_typedef_CtypesPointer(name, ctype, include_cdef):
     destination = ctype.get('destination')
     if not isinstance(destination, dict):
-        _logger.warn('Unsupported base Klass type(destination)=%r', destination)
+        _logger.warn('Unsupported base Klass type(destination)=%r',
+                     destination)
         return False
 
     args = _convert_base_Klass(destination)
@@ -400,7 +435,8 @@ def _convert_typedef(definition, f_out, indent_level, include_cdef=True):
     name = definition.get('name')
     ctype = definition.get('ctype')
     if not name or not isinstance(ctype, dict):
-        _logger.warn('Unknown typedef data name=%r type(ctype)=%r', name, type(ctype))
+        _logger.warn('Unknown typedef data name=%r type(ctype)=%r',
+                     name, type(ctype))
         return False
 
     klass = ctype.get('Klass')
@@ -436,10 +472,16 @@ def _put(f_out, indent_level, *args, **kw):
 def convert(f_in, f_out, import_from='*', indent_level=0):
     unknown_types = set()
 
-    _put(f_out, indent_level, 'cdef extern from ', import_from or '*', ' nogil:')
+    _put(f_out, indent_level,
+         'cdef extern from ', import_from or '*', ' nogil:')
     indent_level += 1
 
-    for definition in load(f_in):
+    if hasattr(f_in, 'read'):
+        definition = load(f_in)
+    else:
+        definition = fin
+
+    for definition in definition:
         if not isinstance(definition, dict):
             continue
 
@@ -458,4 +500,4 @@ def convert(f_in, f_out, import_from='*', indent_level=0):
 
 if __name__ == '__main__':
     basicConfig()
-    convert(stdin, stdout, (len(argv) > 1) and ('"%s"' % argv[1]))
+    convert(stdin, stdout, (len(argv) > 1) and ("'%s'" % argv[1]))
